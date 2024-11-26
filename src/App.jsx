a019b062a7cf00a16eb6920f4a1082ba35aaade1
ajayjain62 <ajay8290440876@gmail.com>
2024-11-26 18:50:34 +0530

    aj

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,54 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Header from './components/Header';
-import Header1 from './components/Header1';
-import shop from './pages/shop';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
-import Product from './pages/Product';
-import Compare from './pages/Compare';
-import Checkout from './pages/checkout';
-import CSR from './pages/CSR';
-import { Button } from '@material-tailwind/react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Header1 from "./components/Header1";
+
+// Import Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop"; // Corrected component name
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Compare from "./pages/Compare";
+import Gastro from './components/Gastro';
+import Checkout from "./pages/Checkout"; // Fixed case sensitivity
+import CSR from "./pages/CSR";
 
 // Import CSS files
-import './assets/css/ionicons.min.css';
-import './assets/css/simple-line-icons.css';
-import './assets/css/plugins/jquery-ui.min.css';
-import './assets/css/bootstrap.min.css';
-import './assets/css/plugins/plugins.css';
-import './assets/css/style.min.css';
+import "./assets/css/ionicons.min.css";
+import "./assets/css/simple-line-icons.css";
+import "./assets/css/plugins/jquery-ui.min.css";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/plugins/plugins.css";
+import "./assets/css/style.min.css";
 
 const App = () => (
-  <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+  <Router>
+    {/* Header Components */}
     <Header />
     <Header1 />
     <Navbar />
+
+    {/* Main Routes */}
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/shop" element={<shop />} />
+      <Route path="/shop" element={<Shop />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/product-page" element={<Product />} />
+      <Route path="/compare" element={<Compare />} />
       <Route path="/csr" element={<CSR />} />
       <Route path="/checkout" element={<Checkout />} />
-
+      <Route path="/gastro" element={<Gastro />} />
     </Routes>
+
+    {/* Footer Component */}
     <Footer />
   </Router>
 );
