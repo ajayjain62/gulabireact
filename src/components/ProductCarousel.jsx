05e32680898436e125c7808a24fb701f54b77fb3
Sumit swami <167609525+sumicodin@users.noreply.github.com>
2024-11-28 15:48:40 +0530

    Update ProductCarousel.jsx
    only hover effect is remaining
    
    Co-Authored-By: Ajay jain <108366050+ajayjain62@users.noreply.github.com>
    Co-Authored-By: surbhi trivedi <116959793+surbhitrivedi@users.noreply.github.com>

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -2527,7 +2527,7 @@ export default function Component() {
                       alignItems: "center",
                       justifyContent: "flex-start",
                       // gap: "1.5rem",
-                      letterSpacing: "0.02em",
+                      letterSpacing: "0.03em",
                       fontFamily: "Lato, sans-serif",
                     }}
                   >
