bb05f3bbd0fd5d99d26fd432ce0f9eedcff72c6d
SameerM30 <sameermaheshwari536@gmail.com>
2024-12-05 17:28:18 +0530

    hdhdh
    Co-Authored-By: surbhi trivedi <116959793+surbhitrivedi@users.noreply.github.com>
    Co-Authored-By: Sumit swami  <167609525+sumicodin@users.noreply.github.com>
    Co-Authored-By: Ajay jain <108366050+ajayjain62@users.noreply.github.com>

--- src/components/BodyDetox/Three.jsx
+++ src/components/BodyDetox/Three.jsx
@@ -82,74 +82,64 @@ const ThreeJsCarousel = () => {
 
   const categories = [
     {
-      src: "//goanutritions.com/cdn/shop/files/82.jpg?v=1732013593&width=1946",
+      src: "https://i.postimg.cc/63GSc1fL/r-alpha.jpg",
       label: "R-Alpha Tablets",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/1_525b7e53-90a1-4cba-8268-15404213f40b.jpg?v=1732079477&width=1946",
+      src: "https://i.postimg.cc/PxwXDyL0/Rhodiola-Rosea.jpg",
       label: "Rhodiola Rosea",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/66.jpg?v=1732011620&width=1946",
+      src: "https://i.postimg.cc/FKYnFn85/lipoic-tablets.jpg",
       label: "Lipo Boost",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/65.jpg?v=1732011997&width=1946",
+      src: "https://i.postimg.cc/3r2rJLQv/Ubiquinol-Tablets.jpg",
       label: "Ubiquinol Tablet",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/62.jpg?v=1732011952&width=1946",
+      src: "https://i.postimg.cc/cLk052wV/ubiquinol-capsules.jpg",
       label: "Ubiquinol Capsules",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/76.jpg?v=1732013242&width=1946",
+      src: "https://i.postimg.cc/wT6zxgkC/NAC.jpg",
       label: "NAC Vitality",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/11_a6adfba9-e4a3-48ba-9577-e3f25dac1b62.jpg?v=1732081409&width=1946",
+      src: "https://i.postimg.cc/WbK5W7t5/TRIPHALA-tablets.jpg",
       label: "Triphala Tablets",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/33.jpg?v=1732079389&width=1946",
+      src: "https://i.postimg.cc/25dT5RGt/noni-juice-tablets.jpg",
       label: "Noni Juice Tablets",
       link: "/category/body-detox",
     },
     {
-      src: "https://goanutritions.com/cdn/shop/products/LUNGSDETOX5.jpg?v=1675936431&width=1946",
+      src: "https://i.postimg.cc/mD8wQY0N/Lung-Detox.jpg",
       label: "Lung Detox tablets",
       link: "/category/body-detox",
     },
     {
-      src: "//goanutritions.com/cdn/shop/files/38.jpg?v=1732014686&width=1946",
+      src: "https://i.postimg.cc/sX4VKXcJ/Liver-detox.jpg",
       label: "Liver Detox Tablets",
       link: "/category/body-detox",
     },
     {
-      src: "https://goanutritions.com/cdn/shop/products/20395c42bb877591cdd49ab5a02d9265.jpg?v=1676114405&width=1946",
-      label: "Grape Seed Extract",
-      link: "/category/height-enhancer",
+      src: "https://i.postimg.cc/SRZmFvH2/SAWPALMETTO5.jpg",
+      label: "Palmetto tablets",
+      link: "/category/body-detox",
     },
     {
-      src: "https://goanutritions.com/cdn/shop/products/a0684901e9f4303e60e4796da488e1c2.jpg?v=1676114377&width=1946",
-      label: "Glutathione Tablets",
-      link: "/category/immunity-booster",
-    },
-    {
-      src: "https://goanutritions.com/cdn/shop/products/8af93504edf5effff2a51eb62663785b.jpg?v=1676114390&width=1946",
-      label: "Skin Whitening",
-      link: "/category/joint-care",
-    },
-    {
-      src: "https://goanutritions.com/cdn/shop/products/N-ACITYLCYSTEINE7.jpg?v=1675923841&width=1946",
-      label: "NAC Supplement",
-      link: "/category/male-vitality",
+      src: "https://i.postimg.cc/V6mwSw80/turmeric-tablet.jpg",
+      label: "Turmeric Tablets",
+      link: "/category/body-detox",
     },
   ];
 
